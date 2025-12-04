f954ffb8b64752ffad9c43a5684c47abf45437be
onoteruaki <terusan.o@icloud.com>
2025-12-04 11:49:58 +0900

    fix format settings

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_format" },
+      },
+      
+      -- 保存時に実行する設定
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+  end,
+}
