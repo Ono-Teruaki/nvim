38c0ac88d6b63d8dc0d2d9d8b9be06e82da3b91e
onoteruaki <terusan.o@icloud.com>
2025-12-08 18:02:26 +0900

    change default color scheme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,17 +1,17 @@
 return {
-    {
-        "cocopon/iceberg.vim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme iceberg]])
-        end,
-    },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 
-    -- statusline
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = true,
-    }
+	-- statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = true,
+	},
 }
