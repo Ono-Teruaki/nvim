6abec2e8a9f356d38b0d1e50302ac388d4652db7
OnoTeruaki <teruaki_ono@demia.co.jp>
2026-01-21 16:36:43 +0900

    add remote-nvim

---
+++ lua/plugins/remote-nvim.lua
@@ -0,0 +1,10 @@
+return {
+	"amitds1997/remote-nvim.nvim",
+	version = "*", -- 最新の安定版を使用
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-telescope/telescope.nvim", -- 接続先の選択に使用
+	},
+	config = true,
+}
