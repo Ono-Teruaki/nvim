72e10c7747c5e6cc401bc45ea16542079ef19a95
OnoTeruaki <teruaki_ono@demia.co.jp>
2026-04-07 17:46:10 +0900

    Neotreeのデフォルト表示を修正

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,23 @@
 -- neo-tree.lua
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    requires = {
-        'nvim-lua/plenary.nvim',
-        'kyazdani42/nvim-web-devicons', -- ファイルアイコン用（オプション）
-        'MunifTanjim/nui.nvim',
-    },
-    config = function()
-        require("neo-tree").setup({
-            window = {
-                width = 35
-            }
-        })
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	requires = {
+		"nvim-lua/plenary.nvim",
+		"kyazdani42/nvim-web-devicons", -- ファイルアイコン用（オプション）
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				width = 35,
+			},
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
+	end,
 }
